ad944acf60d421a39c050f4e30c7f684c4b421a2
Tomas Schier <tomas@xpsolutions.com.au>
2023-07-27 16:53:29 +1000

    Doc updates

--- test-data-adr/src/app.ts
+++ test-data-adr/src/app.ts
@@ -1,28 +1,53 @@
 import express, { request }  from 'express';
 import {NextFunction, Request, Response} from 'express';
 import bodyParser from 'body-parser';
-import * as dotenv from 'dotenv'; 
+//import * as dotenv from 'dotenv'; 
+
+import * as https from 'https'
+import path from 'path';
+import { readFileSync } from 'fs';
 
 
-
-dotenv.config();
+// dotenv.config();
 console.log(JSON.stringify(process.env, null, 2))
 
 const dbHost = `${process.env.DB_HOST}`
 const dbPort = 3005;
-
-const exp = express;
 const app = express();
-const port = 3005;
 
-// get billing for a number of accounts
+async function startServer() {
+  const certFile = path.join(__dirname, '/certificates/mtls-server.pem')
+  const keyFile = path.join(__dirname, '/certificates/mtls-server.key')
+  const rCert = readFileSync(certFile, 'utf8');
+  const rKey = readFileSync(keyFile, 'utf8');
+  const otions = {
+    key: rKey,
+    cert: rCert
+  }
+  https.createServer(otions, app)
+    .listen(dbPort, () => {
+      console.log('Server started');
+    });
+}
+ 
+startServer();
+
+// get the jwks signing key. This is called by the auth server
 app.get(`/jwks`, async (req: Request, res: Response, next: NextFunction) => {
-    console.log(`Received request on ${port} for ${req.url}`);
-    let result = 'Ades123'
-    res.send(result);
+    console.log(`Received request on ${dbPort} for ${req.url}`);
+    res.contentType('application/json')
+    const jwkFile = path.join(__dirname, '/data/jwk.json')
+    const jwk = JSON.parse(readFileSync(jwkFile, 'utf8'));
+    console.log(jwk);
+    res.send(jwk);
 });
 
-app.listen(port, dbHost, () => {
-    console.log(`Server running at http://${dbHost}:${dbPort}/`);
-    console.log('Listening for requests....');
+app.get(`/test`, async (req: Request, res: Response, next: NextFunction) => {
+    console.log(`Received request on ${dbPort} for ${req.url}`);
+    res.send('Hello World');
+});
+
+app.get(`/callback`, async (req: Request, res: Response, next: NextFunction) => {
+  console.log(`Received callback on ${dbPort} for ${req.url}`);
+  res.send('Authenticationcomplete');
 });
