2fbbee4a08c4f0b210615a4f62267861badfd96f
Tomas Schier <tomas@xpsolutions.com.au>
2023-09-06 17:13:52 +1000

    Removed ADR code, to new branch

--- test-data-adr/src/models/user.ts
+++
@@ -1,8 +0,0 @@
-export interface CdrUser {
-    customerId: string; // the internal customer id used by the DH
-    loginId: string; // the unique login used for authorisation, which is returned as userId 
-    encodeUserId: string;
-    encodedAccounts: string[] | undefined;
-    accounts: string[] | undefined;
-
-}
